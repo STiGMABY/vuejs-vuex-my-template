68a487bb54718db243a846c52faad0a055b706a5
stigmaby@gmail.com <oFFline13>
2021-01-14 21:29:07 +0300

    Component 'Navbar' was created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <v-container fluid> <!--  fluid - файл растянеться на всю ширину-->
+            <v-app-bar
+                    color="teal"
+                    dark
+            >
+                <v-spacer/>  <!--заполнит все пространство справа и сдвинет кнопки влево-->
+                <v-btn outlined class="ml-5">Логин</v-btn>
+                <v-btn outlined class="ml-5">Главная</v-btn>
+            </v-app-bar>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
+
